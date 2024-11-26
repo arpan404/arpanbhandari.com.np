13be0aef6531ea2796085d3e81b05166c17e7712
Arpan Bhandari <itsmearpan1@gmail.com>
2024-11-26 13:09:28 -0600

    Made hero section

--- components/sections/hero/index.tsx
+++ components/sections/hero/index.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Intro from "./intro";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <div className="w-full flex justify-center">
-      <main className="container">
+    <div className="w-full flex justify-center h-[100dvh]">
+      <main className="container flex justify-between items-center flex-wrap">
         <Intro />
+        <div>
+          <div className="flex items-center">
+            <Image
+              src={"/images/hero.png"}
+              height={500}
+              width={500}
+              alt="Arpan Bhandari"
+              className="z-0 rounded-full"
+            />
+          </div>
+        </div>
       </main>
     </div>
   );
