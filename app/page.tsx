39dcba825955979454301bb203c468afee9260ea
Arpan Bhandari <itsmearpan1@gmail.com>
2024-12-11 11:25:16 -0600

    initialize the project

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import HeroSection from "@/components/sections/hero";
-
-export default function Home() {
-  return (
-    <>
-      <HeroSection />
-    </>
-  );
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,22 @@
 import type { Config } from "tailwindcss";
-import { default as flattenColorPalette } from "tailwindcss";
 
-const config = {
-  darkMode: ["class"],
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
-  prefix: "",
-  theme: {},
-  plugins: [addVariablesForColors],
+export default {
+  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+    },
+  },
+  plugins: [],
 } satisfies Config;
-function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
-
-  addBase({
-    ":root": newVars,
-  });
-}
-export default config;
