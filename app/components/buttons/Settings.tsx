5710f2659f4f92857e1025ba0b4782d645f7c218
Arpan Bhandari <itsmearpan1@gmail.com>
2024-12-11 20:39:37 -0600

    #refactor: Separate different setting options to array

--- app/components/buttons/Settings.tsx
+++ app/components/buttons/Settings.tsx
@@ -10,10 +10,10 @@ import {
   Sun,
   Moon,
   Music,
-  SpeakerIcon,
   VolumeOffIcon,
 } from "lucide-react";
 import { useState } from "react";
+import type { Theme as ThemeType } from "~/helpers/types";
 import useTheme from "~/hooks/useTheme";
 
 export default function Settings() {
@@ -38,50 +38,71 @@ export default function Settings() {
       </DropdownMenuTrigger>
 
       <DropdownMenuContent className="py-3 px-2 w-60 relative">
-        <Button
-          onClick={() => changeTheme("system")}
-          variant="ghost"
-          disabled={currentTheme === "system"}
-          className={`h-8 w-8 rounded-full p-2`}
-        >
-          <Monitor />
-        </Button>
+        {themeButtons.map((button) => (
+          <Button
+            key={button.label}
+            onClick={() => changeTheme(button.label)}
+            variant="ghost"
+            disabled={currentTheme === button.label}
+            className={`h-8 w-8 rounded-full p-2`}
+          >
+            {button.icon}
+            <span className="sr-only">{button.srOnly}</span>
+          </Button>
+        ))}
 
-        <Button
-          onClick={() => changeTheme("light")}
-          variant="ghost"
-          disabled={currentTheme === "light"}
-          className="h-8 w-8 rounded-full p-2"
-        >
-          <Sun />
-        </Button>
-
-        <Button
-          onClick={() => changeTheme("dark")}
-          variant="ghost"
-          disabled={currentTheme === "dark"}
-          className="h-8 w-8 rounded-full p-2"
-        >
-          <Moon />
-        </Button>
-
-        <Button
-          variant="ghost"
-          onClick={toggleMusic}
-          disabled={music}
-          className="h-8 w-8 rounded-full p-2"
-        >
-          <Music />
-        </Button>
-        <Button
-          variant="ghost"
-          onClick={toggleMusic}
-          disabled={!music}
-          className="h-8 w-8 rounded-full p-2"
-        >
-          <VolumeOffIcon />
-        </Button>
+        {musicButtons.map((button) => (
+          <Button
+            key={button.label}
+            variant="ghost"
+            onClick={toggleMusic}
+            disabled={music}
+            className="h-8 w-8 rounded-full p-2"
+          >
+            {button.icon}
+            <span className="sr-only">{button.srOnly}</span>
+          </Button>
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
   );
 }
+
+const themeButtons: Array<{
+  icon: React.ReactNode;
+  label: ThemeType;
+  srOnly: string;
+}> = [
+  {
+    icon: <Monitor />,
+    label: "system",
+    srOnly: "Use System Theme",
+  },
+  {
+    icon: <Sun />,
+    label: "light",
+    srOnly: "Use Light Theme",
+  },
+  {
+    icon: <Moon />,
+    label: "dark",
+    srOnly: "Use Dark Theme",
+  },
+];
+
+const musicButtons: Array<{
+  icon: React.ReactNode;
+  label: string;
+  srOnly: string;
+}> = [
+  {
+    icon: <Music />,
+    label: "music-on",
+    srOnly: "Turn Music On",
+  },
+  {
+    icon: <VolumeOffIcon />,
+    label: "music-off",
+    srOnly: "Turn Music Off",
+  },
+];
