f020be24544900c9b240bfa5c83672bc1808e00d
Arpan Bhandari <itsmearpan1@gmail.com>
2024-12-12 23:37:08 -0600

    improve: Made animation better

--- app/components/common/AnimatedBackground.tsx
+++ app/components/common/AnimatedBackground.tsx
@@ -22,7 +22,7 @@ export default function AnimatedBackground() {
   const particlesOptions: ISourceOptions = {
     particles: {
       number: {
-        value: 50,
+        value: 80,
         density: {
           enable: true,
         },
