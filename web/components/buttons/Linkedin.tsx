ab1e36b31d57aa136512ed7382c5345a3b24b10d
Arpan Bhandari <itsmearpan1@gmail.com>
2024-12-19 14:45:09 -0600

    #fixed: Made github and linkedin buttons better

--- web/components/buttons/Linkedin.tsx
+++ web/components/buttons/Linkedin.tsx
@@ -7,7 +7,7 @@ export default function Linkedin({ url }: { url: string }) {
       <button className="group hover:bg-sky-600 relative bg-sky-700 rounded-full text-neutral-50 duration-500 font-bold flex justify-start gap-2 items-center py-2 px-6 cursor-pointer">
         <AiFillLinkedin className="text-3xl" />
         <span className="border-l-2 px-2 border-gray-400">arpan404</span>
-        <div className="group-hover:opacity-100 opacity-0 top-16 absolute z-10 inline-block px-3 py-2 text-xs font-medium text-white transition-opacity duration-500 bg-sky-600 rounded-lg shadow-sm before:w-3 before:h-3 before:rotate-45 before:-top-1 before:left-20 before:bg-sky-600 before:absolute">
+        <div className="group-hover:opacity-100 opacity-0 top-14 absolute z-10 inline-block px-3 py-2 text-xs font-medium text-white transition-opacity duration-500 bg-sky-600 rounded-lg shadow-sm before:w-3 before:h-3 before:rotate-45 before:-top-1 before:left-20 before:bg-sky-600 before:absolute">
           See my profile!
         </div>
       </button>
