d9605ff35a1ff9e3624b3c022ff5e1d60eb3f522
Arpan Bhandari <itsmearpan1@gmail.com>
2024-12-19 18:07:49 -0600

    #refactor: Made andy button size relevant to the surrounding elements

--- web/components/common/Andy.tsx
+++ web/components/common/Andy.tsx
@@ -9,7 +9,7 @@ import {
 export default function Andy({ buttonText }: Readonly<{ buttonText: string }>) {
   return (
     <Sheet>
-      <SheetTrigger className="text-xs px-6 py-3 rounded-full font-semibold text-pretty cursor-pointer dark:bg-[#ff7d37] bg-[#ff6730] hover:dark:bg-[#ff7d37] hover:bg-[#ff6730] hover:cursor-pointer saturate-[110%] hover:saturate-[130%] active:opacity-50 transition-all delay-0 ease-linear m-0">
+      <SheetTrigger className="text-xs px-6 py-[0.6rem] rounded-full font-semibold text-pretty cursor-pointer dark:bg-[#ff7d37] bg-[#ff6730] hover:dark:bg-[#ff7d37] hover:bg-[#ff6730] hover:cursor-pointer saturate-[110%] hover:saturate-[130%] active:opacity-50 transition-all delay-0 ease-linear m-0">
         <h3 className="text-xs font-semibold text-white">{buttonText}</h3>
       </SheetTrigger>
       <SheetContent className="w-full sm:w-[400px] md:w-[500px] lg:w-[600px]">
