3a65205dee45616b704bf557cc1ee32dddd4bdf8
Arpan Bhandari <itsmearpan1@gmail.com>
2024-12-19 18:34:43 -0600

    #refactor: MAde andy more easier to reuse

--- web/components/common/Andy.tsx
+++ web/components/common/Andy.tsx
@@ -5,11 +5,20 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet';
+import { cn } from '@/lib/utils';
 
-export default function Andy({ buttonText }: Readonly<{ buttonText: string }>) {
+export default function Andy({
+  buttonText,
+  className,
+}: Readonly<{ buttonText: string; className?: string }>) {
   return (
     <Sheet>
-      <SheetTrigger className="text-xs px-6 py-[0.6rem] rounded-full font-semibold text-pretty cursor-pointer dark:bg-[#ff7d37] bg-[#ff6730] hover:dark:bg-[#ff7d37] hover:bg-[#ff6730] hover:cursor-pointer saturate-[110%] hover:saturate-[130%] active:opacity-50 transition-all delay-0 ease-linear m-0">
+      <SheetTrigger
+        className={cn(
+          'text-xs px-6 py-[0.6rem] rounded-full font-semibold text-pretty cursor-pointer dark:bg-[#ff7d37] bg-[#ff6730] hover:dark:bg-[#ff7d37] hover:bg-[#ff6730] hover:cursor-pointer saturate-[110%] hover:saturate-[130%] active:opacity-50 transition-all delay-0 ease-linear m-0',
+          className
+        )}
+      >
         <h3 className="text-xs font-semibold text-white">{buttonText}</h3>
       </SheetTrigger>
       <SheetContent className="w-full sm:w-[400px] md:w-[500px] lg:w-[600px]">
--- web/components/sections/About.tsx
+++ web/components/sections/About.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import Andy from '../common/Andy';
+
 export default function About() {
   return (
     <section className="py-10 bg-background px-4">
@@ -9,8 +12,39 @@ export default function About() {
           </span>
         </h2>
       </div>
-      <div className="flex justify-center">
-        <div className="container"></div>
+      <div className="flex justify-center mt-5">
+        <div className="container flex justify-between items-center flex-wrap gap-10">
+          <div className="flex items-center justify-center md:flex-1 w-full ">
+            <Image
+              src={'/images/thinking.png'}
+              alt="Thinking"
+              width={400}
+              height={400}
+              className="rounded-full"
+            />
+          </div>
+          <div className="md:flex-1 space-y-2 md:space-y-8">
+            <p className="text-base">
+              I am a dedicated scholar, a fervent tech enthusiast, and an adept
+              software developer with a passion for unraveling complex problems
+              using an array of programming tools and languages. My affinity for
+              technology propels me into the realm of new innovations, while my
+              unwavering commitment to the craft fuels my continuous journey as
+              a developer. Fueled by a perpetual eagerness to learn and adapt to
+              the dynamic tech landscape, I thrive on the challenges that
+              accompany staying at the forefront of the ever-evolving digital
+              realm.
+            </p>
+            <div>
+              <p className="text-base">
+                Wanna learn more about me? Why not ask my assistant, Andy?
+              </p>
+              <div>
+                <Andy buttonText="Chat with Andy" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
