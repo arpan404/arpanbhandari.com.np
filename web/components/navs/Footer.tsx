76db3c70b3d785537cf10528f2890f8d7f265c26
Arpan Bhandari <itsmearpan1@gmail.com>
2024-12-22 12:06:47 -0600

    #refactor: Made footer better

--- web/components/navs/Footer.tsx
+++ web/components/navs/Footer.tsx
@@ -1,7 +1,16 @@
+import { Copyright } from 'lucide-react';
+
 export default function Footer() {
   return (
-    <footer className="bg-background py-8 md:py-12">
-      <div></div>
+    <footer className="bg-background py-4 flex justify-center drop-shadow-sm">
+      <div className="">
+        <div className="flex gap-1 text-sm items-center text-center text-primary">
+          <Copyright size={14} /> Copyright 2025 Arpan Bhandari
+        </div>
+        <div className="flex justify-center text-xs font-medium relative top-1 opacity-80">
+          <span className="">All rights reserved.</span>
+        </div>
+      </div>
     </footer>
   );
 }
