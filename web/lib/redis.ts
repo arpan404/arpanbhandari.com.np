661ee847af07a44e2070a3f97a24e3e4ccceba33
Arpan Bhandari <itsmearpan1@gmail.com>
2024-12-25 17:13:19 -0600

    #feat: Added redis for caching the query

---
+++ web/lib/redis.ts
@@ -0,0 +1,12 @@
+import Redis from 'ioredis';
+
+const redis = new Redis({
+  host: process.env.NEXT_REDIS_HOST || '127.0.0.1',
+  port: process.env.NEXT_REDIS_PORT
+    ? Number(process.env.NEXT_REDIS_PORT)
+    : 6379,
+  password: process.env.NEXT_REDIS_PASSWORD || '',
+  db: Number(process.env.NEXT_REDIS_DB) || 0,
+});
+
+export default redis;
