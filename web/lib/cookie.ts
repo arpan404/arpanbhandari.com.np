18136cfca88260969c086e70261147e1e33916f8
Arpan Bhandari <itsmearpan1@gmail.com>
2024-12-25 16:27:59 -0600

    #fix : added error handler in cookie handler

--- web/lib/cookie.ts
+++ web/lib/cookie.ts
@@ -1,18 +1,23 @@
 export function getCookie(name: string) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) {
-    return parts.pop()?.split(';').shift() || null;
+  try {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+      return parts.pop()?.split(';').shift() || null;
+    }
+    return null;
+  } catch (e) {
+    console.error(e);
+    return null;
   }
-  return null;
 }
-
 export function addCookie(name: string, value: string, days: number = 7) {
-  const expirationDate = new Date();
-  expirationDate.setDate(expirationDate.getDate() + days);
-  document.cookie = `${name}=${value}; expires=${expirationDate.toUTCString()}; path=/`;
-}
-
-export function deleteCookie(name: string) {
-  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+  try {
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + days);
+    document.cookie = `${name}=${value}; expires=${expirationDate.toUTCString()}; path=/`;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 }
