467c5a5be65588895d463c34368f006b006414fa
Arpan Bhandari <itsmearpan1@gmail.com>
2024-12-26 21:10:13 -0600

    #add: Added not found page

--- web/app/not-found.tsx
+++ web/app/not-found.tsx
@@ -1,11 +1,31 @@
+import AnimatedBackground from '@/components/common/AnimatedBackground';
+import { Button } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
 
-export default async function NotFound() {
+export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <main className="bg-transparent pt-[52px] h-[calc(100dvh-68px)] flex items-center justify-center">
+      <div className="container px-2">
+        <AnimatedBackground />
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-6xl sm:text-8xl md:text-[10rem] font-bold text-center dark:text-[#ff7d37] text-[#ff6730] saturate-[110%]">
+            404
+          </h1>
+          <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold text-center text-primary">
+            Page Not Found
+          </h2>
+          <p className="text-center text-primary mt-2 md:mt-4 text-sm sm:text-base md:text-lg">
+            Oops! It seems the page you're looking for has taken a detour or
+            decided to retire.
+          </p>
+          <Link href="/" className='mt-2 md:mt-4'>
+            <Button className='px-4 md:px-6 rounded-full' variant={'outline'}>
+              <ArrowLeft /> Go Back Home
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 }
