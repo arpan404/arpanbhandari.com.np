d74952e44c99ec83e00fb48ad4e1b34f3bf6bc35
Arpan Bhandari <itsmearpan1@gmail.com>
2025-01-03 15:03:14 -0600

    #refactor: Added types to technologies tooltip

--- web/components/common/TechnologiesTooltip.tsx
+++ web/components/common/TechnologiesTooltip.tsx
@@ -33,65 +33,69 @@ export default function TechnologiesTooltip({
 
   return (
     <div className="flex flex-row items-center justify-start w-full flex-wrap gap-1">
-      {data.map((item, idx) => (
-        <div
-          className="-mr-2  relative group"
-          key={item.skill.uid}
-          onMouseEnter={() => setHoveredIndex(item.skill.uid)}
-          onMouseLeave={() => setHoveredIndex(null)}
-        >
-          <AnimatePresence mode="popLayout">
-            {hoveredIndex === item.skill.uid && (
-              <motion.div
-                initial={{ opacity: 0, y: 20, scale: 0.6 }}
-                animate={{
-                  opacity: 1,
-                  y: 0,
-                  scale: 1,
-                  transition: {
-                    type: 'spring',
-                    stiffness: 260,
-                    damping: 10,
-                  },
-                }}
-                exit={{ opacity: 0, y: 20, scale: 0.6 }}
-                style={{
-                  translateX: translateX,
-                  rotate: rotate,
-                  whiteSpace: 'nowrap',
-                }}
-                className="absolute -top-8 -left-1/2 translate-x-1/2 flex text-xs  flex-col items-center justify-center rounded-full bg-primary z-50 shadow-xl px-2 py-1"
-              >
-                <div className="absolute inset-x-5 z-[202] w-[20%] -bottom-px bg-gradient-to-r from-transparent via-orange-500 to-transparent h-px " />
-                <div className="absolute left-4 w-[40%] z-[202] -bottom-px bg-gradient-to-r from-transparent via-red-500 to-transparent h-px " />
-                <div className="font-semibold text-background relative z-[202] text-xs">
-                  {item.skill.name}
-                </div>
-              </motion.div>
-            )}
-          </AnimatePresence>
+      {data.map(
+        (item: {
+          skill: { uid: string; name: string; logo?: { url: string } };
+        }) => (
           <div
-            className={`w-[40px] h-[40px] max-w-[40px] max-h-[40px] overflow-hidden cursor-pointer group-hover:scale-105 rounded-full flex justify-center items-center p-[6px] group-hover:z-30  relative transition duration-500 border-[1px] border-secondary bg-muted drop-shadow-xl`}
+            className="-mr-2  relative group"
+            key={item.skill.uid}
+            onMouseEnter={() => setHoveredIndex(item.skill.uid)}
+            onMouseLeave={() => setHoveredIndex(null)}
           >
-            <Image
-              onMouseMove={handleMouseMove}
-              height={100}
-              width={100}
-              priority={true}
-              draggable={false}
-              src={
-                item.skill.logo
-                  ? `${process.env.NEXT_PUBLIC_STRAPI_URL}${item.skill.logo.url}`
-                  : '/images/image-not-found-icon.png'
-              }
-              alt={item.skill.name}
-              className={`w-auto h-auto max-h-full max-w-full object-cover !m-0 !p-0 object-top rounded-md ${
-                item.skill.logo ? '' : 'dark:invert !p-1'
-              }`}
-            />
+            <AnimatePresence mode="popLayout">
+              {hoveredIndex === item.skill.uid && (
+                <motion.div
+                  initial={{ opacity: 0, y: 20, scale: 0.6 }}
+                  animate={{
+                    opacity: 1,
+                    y: 0,
+                    scale: 1,
+                    transition: {
+                      type: 'spring',
+                      stiffness: 260,
+                      damping: 10,
+                    },
+                  }}
+                  exit={{ opacity: 0, y: 20, scale: 0.6 }}
+                  style={{
+                    translateX: translateX,
+                    rotate: rotate,
+                    whiteSpace: 'nowrap',
+                  }}
+                  className="absolute -top-8 -left-1/2 translate-x-1/2 flex text-xs  flex-col items-center justify-center rounded-full bg-primary z-50 shadow-xl px-2 py-1"
+                >
+                  <div className="absolute inset-x-5 z-[202] w-[20%] -bottom-px bg-gradient-to-r from-transparent via-orange-500 to-transparent h-px " />
+                  <div className="absolute left-4 w-[40%] z-[202] -bottom-px bg-gradient-to-r from-transparent via-red-500 to-transparent h-px " />
+                  <div className="font-semibold text-background relative z-[202] text-xs">
+                    {item.skill.name}
+                  </div>
+                </motion.div>
+              )}
+            </AnimatePresence>
+            <div
+              className={`w-[40px] h-[40px] max-w-[40px] max-h-[40px] overflow-hidden cursor-pointer group-hover:scale-105 rounded-full flex justify-center items-center p-[6px] group-hover:z-30  relative transition duration-500 border-[1px] border-secondary bg-muted drop-shadow-xl`}
+            >
+              <Image
+                onMouseMove={handleMouseMove}
+                height={100}
+                width={100}
+                priority={true}
+                draggable={false}
+                src={
+                  item.skill.logo
+                    ? `${process.env.NEXT_PUBLIC_STRAPI_URL}${item.skill.logo.url}`
+                    : '/images/image-not-found-icon.png'
+                }
+                alt={item.skill.name}
+                className={`w-auto h-auto max-h-full max-w-full object-cover !m-0 !p-0 object-top rounded-md ${
+                  item.skill.logo ? '' : 'dark:invert !p-1'
+                }`}
+              />
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      )}
     </div>
   );
 }
