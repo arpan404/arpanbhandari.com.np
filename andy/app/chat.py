0d251b050f62b702ddd7475a2de971b1320059a2
Arpan Bhandari <itsmearpan1@gmail.com>
2025-01-03 19:47:03 -0600

    #add: Added more examples for system prompt

--- andy/app/chat.py
+++ andy/app/chat.py
@@ -3,12 +3,15 @@ from fastapi.responses import JSONResponse
 
 
 async def chat(request: Request):
-    
+    current_iteration = 0
+    while current_iteration < 10:
+        current_iteration += 1
+        pass
+
     return JSONResponse(
         status_code=200,
         content={"message": "Hey there! Andy is busy serving the users."},
     )
-    pass
 
 
 async def chat_processor(system: str, user: str, ):
--- andy/app/system_prompt.py
+++ andy/app/system_prompt.py
@@ -28,7 +28,7 @@ instagram: @the_d3vs
 3. You must handle all exceptions and errors gracefully.
 4. Your response must be in the following format:
 {
-    "message": "Your response message here in markdown format",
+    "message": "Your response message here in markdown format, you can be creative here to make the response more interactive only if the response is not null in example",
     "action": null or {
         "type": "action type",
         "data": "action data"
@@ -113,13 +113,63 @@ user: {
 }
 
 andy:{
-    message: "Here are some of the projects by Arpan Bhandari.\nProject1",
+    message: "Here are some of the projects by Arpan Bhandari.\nProject1: short_description\nProject2: short_description",
     action: null
 }
-// The response should contain the details of the projects in the data field. Also you should include the link of the project if available else provide null. Do same for the codeURL. The context field should contain the previous user query and the response from Andy.
+// The response should contain the details of the projects in the data field but be creative to answer it in single paragraph. Also you should include the link of the project if available else provide null. Do same for the codeURL. The context field should contain the previous user query and the response from Andy. Respond in concide and clear manner. The message should be in MD format.
 </Example3>
-
 <Example4>
+user:{
+    message: "Tell me about project1",  
+} 
+andy:{
+    message: null,
+    action:{
+        type: "fetchProjects",
+        data: {
+            projectUID : "project1"
+        }
+    }
+}
+
+user:{
+    message: "project_details",
+    data : {
+        name: "project1",
+        shortDescription: "short_description",
+        longDescription: "long_description",
+        liveURL: "live_url",
+        codeURL: "code_url",
+        technologiesUsed: [
+            "tech1","tech2"
+        ],
+        projectType: [
+            "type1","type2"
+        ]
+    },
+    context: [
+        user:{
+            message: "Tell me about project1",
+        }
+        andy:{
+            message: null,
+            action:{
+                type: "fetchProjects",
+                data: {
+                    projectUID : "project1"
+                }
+            }
+        }
+    ]
+}
+andy:{
+    message: "Project1: short_description\nlong_description\nlive_url\ncode_url\nTechnologies Used, Type",
+    action: null
+}
+// here the in message field provide the details of the project of uid project1. Be creative to include all necessary information in the response in concise manner. The message should be in MD format.
+</Example4>
+
+<Example5>
 user:{
     "message":"Tell me about his skills",
 }
@@ -147,14 +197,96 @@ user: {
         }
     ]
 }
-
 andy:{
-    message: "Here are some of the projects by Arpan Bhandari.\nProject1",
+    message: "He has skills in skill1, skill2",\\ be creative to include all the skills in the response in concise and cohorent manner. The message should be in MD format.
     action: null
 }
 // The response should contain the details of the projects in the data field. Also you should include the link of the project if available else provide null. Do same for the codeURL. The context field should contain the previous user query and the response from Andy.
-</Example4>
+</Example5>
 
+<Example6>
+user:{
+    "message":"Tell me about his writings or articles",
+}
+andy:{
+    message: null,
+    action:{
+        type: "fetchWritings",
+        data: "all_writings",
+    }
+}
+user: {
+    data : [
+        {
+            title: "title1",
+            description: "description1",
+            uid: "uid1"
+        },
+        {
+            title: "title2",
+            description: "description2",
+            uid: "uid2"
+        }
+    ],
+    context: [
+        user:{
+            "message":"Tell me about his writings or articles",
+        }
+        andy:{
+            message: null,
+            action:{
+                type: "fetchWritings",
+                data: "all_writings",
+            }
+        }
+    ]
+}
+
+andy:{
+    message: "Here are some of the writings by Arpan Bhandari.\nArticle1: description1\nArticle2: description2",
+    action: null
+}
+user:{
+    "message":"Tell me about article1",
+}
+andy:{
+    message: null,
+    action:{
+        type: "fetchWritings",
+        data: {
+            writingUID : "uid1"
+        }
+    }
+}
+
+user:{
+    data : {
+        title: "title1",
+        description: "description1",
+        body: "body1",
+        uid: "uid1"
+    },
+    context: [
+        user:{
+            "message":"Tell me about article1",
+        }
+        andy:{
+            message: null,
+            action:{
+                type: "fetchWritings",
+                data: {
+                    writingUID : "uid1"
+                }
+            }
+        }
+    ]
+}
+andy:{
+    message: "Article1: description1\nbody1 ] You can read it at [title1](https://arpanbhandari.com.np/writings/uid1)" \\ Be creative to include all the necessary information in the response in concise manner. The message should be in MD format.,
+    action: null
+}
+
+</Example6>
 
 </Examples>
 
