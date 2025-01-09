4b6656bfab95471d1166abc16ff638ec1134a459
Arpan Bhandari <itsmearpan1@gmail.com>
2025-01-09 04:01:07 -0600

    #refactor: Made redis operate through url

--- web/lib/redis.ts
+++ web/lib/redis.ts
@@ -1,13 +1,7 @@
 import Redis from 'ioredis';
 
+const redisURL = process.env.REDIS_URL!;
 // Create a new Redis instance to connect to the Redis server and use it in the app
-const redis = new Redis({
-   host: process.env.NEXT_REDIS_HOST || '127.0.0.1',
-   port: process.env.NEXT_REDIS_PORT
-      ? Number(process.env.NEXT_REDIS_PORT)
-      : 6379,
-   password: process.env.NEXT_REDIS_PASSWORD || '',
-   db: Number(process.env.NEXT_REDIS_DB) || 0,
-});
+const redis = new Redis(redisURL);
 
 export default redis;
