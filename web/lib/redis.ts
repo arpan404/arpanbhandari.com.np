4d33a614be4c755194ad1478983ffb75dcd92164
Arpan Bhandari <itsmearpan1@gmail.com>
2025-01-09 04:34:09 -0600

    #chore: test

--- web/lib/redis.ts
+++ web/lib/redis.ts
@@ -1,6 +1,6 @@
 import Redis from 'ioredis';
 
-const redisURL = process.env.REDIS_URL!;
+const redisURL = process.env.NEXT_REDIS_URL || 'redis://localhost:6379';
 // Create a new Redis instance to connect to the Redis server and use it in the app
 const redis = new Redis(redisURL);
 
