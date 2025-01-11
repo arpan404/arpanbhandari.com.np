0519a9fc43dfd05d3ede5f0868ae5c12ea3de82c
Arpan Bhandari <itsmearpan1@gmail.com>
2025-01-10 21:48:42 -0600

    #fix: fixed time zone problem

--- web/lib/date.ts
+++ web/lib/date.ts
@@ -1,18 +1,18 @@
 /**
- * Format a timestamp to a human-readable date.
+ * Format a timestamp to a human-readable date in local time.
  * @param isoTimestamp - The timestamp to format.
- * @param timezone - The timezone to format the timestamp in.
  * @returns {string} - The formatted date. Eg. 'Jan 1, 2025'.
  */
-const formatTimestamp = (isoTimestamp: string, timezone = 'UTC') => {
+const formatTimestamp = (isoTimestamp: string): string => {
    const date = new Date(isoTimestamp);
    const options: Intl.DateTimeFormatOptions = {
-      timeZone: timezone,
       year: 'numeric',
       month: 'short',
       day: 'numeric',
    };
    const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
+   console.log(isoTimestamp);
+   console.log(formattedDate);
    return formattedDate;
 };
 
